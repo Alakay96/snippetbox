46214ea97a2889523b4c3cd6b2a4b8f2df26a9f5
Alex <alex_jj96@hotmail.com>
2025-07-05 16:35:43 +1000

    fix view.tmpl linking errors

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,8 +4,7 @@ import "net/http"
 
 func (app *application) routes() *http.ServeMux {
 	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static"))
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("GET /{$}", app.home)
