688babedccf5d3f0934510925e7e4e3dfccd421d
Alex <alex_jj96@hotmail.com>
2025-07-12 22:18:09 +1000

    correct blank form errors

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -56,9 +56,9 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 type snippetCreateForm struct {
-	Title               string
-	Content             string
-	Expires             int
+	Title               string `form:"title"`
+	Content             string `form:"content"`
+	Expires             int    `form:"expires"`
 	validator.Validator `form:"-"`
 }
 
