f5a16d9f64a674e206c6653be0e62fdbd753c95b
Alex <alex_jj96@hotmail.com>
2025-07-21 16:07:21 +1000

    embedding static files 12.1

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,12 +4,12 @@ import (
 	"net/http"
 
 	"github.com/justinas/alice"
+	"snippetbox.alex.net/ui"
 )
 
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+	mux.Handle("GET /static/", http.FileServerFS(ui.Files))
 
 	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 
